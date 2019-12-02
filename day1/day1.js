9da07941a3fb23a9e382ccd1f036d352594a43cb
Hüseyin Dereli <hdereli@gmail.com>
2019-12-02 08:38:09 +0300

    day1

---
+++ day1/day1.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const filename = 'day1/input.txt';
+
+function getResult(){
+    // Get Input
+    const inputArray = fs.readFileSync(filename).toString().split("\n");
+    // Reduce
+    var sumOfAll = inputArray.reduce(requiredFuel,0);
+    // Display Result
+    console.log(sumOfAll);
+}
+
+function requiredFuel(allFuel, item, index) {
+    return allFuel + Math.floor( parseFloat(item) / 3 ) - 2;
+}
+
+getResult();
